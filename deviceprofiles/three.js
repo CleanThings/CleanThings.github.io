187eee295f94ca552e2b5ba1fd127ad7dd439417
CleanThings <cleanthingsglobal@gmail.com>
2023-09-29 20:15:40 +0900

    Three Test

--- deviceprofiles/three.js
+++ deviceprofiles/three.js
@@ -1,32 +1,32 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+
 
 const canvas = document.querySelector('#webgl');
 const renderer = new THREE.WebGLRenderer( { canvas: canvas, antialias: true } );
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.outputColorSpace = THREE.SRGBColorSpace;
-//renderer.outputEncoding = THREE.sRGBEncoding;
-//https://discourse.threejs.org/t/updates-to-color-management-in-three-js-r152/50791
-
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight);
-camera.position.set( 0, 1, 10 );
 
 const scene = new THREE.Scene()
-scene.background = new THREE.Color( 0xbfe3dd );
+scene.background = new THREE.Color( 0xf2f2f2 );
+
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight);
+camera.position.set( 0, 2, 4);
 
 const loader = new GLTFLoader();
-loader.load('shiba.glb', function(gltf) {scene.add(gltf.scene);})
+loader.load('SmartThingsMotionSensor.glb', function(gltf) {scene.add(gltf.scene);})
 
-const light1 = new THREE.PointLight( 0xffffff, 20, 100 );
-light1.position.set( 50, 30, 50 );
+const light1 = new THREE.PointLight( 0xffffff, 40, 100 );
+light1.position.set( 2, 3, 5 );
 scene.add( light1 );
 
-const light2 = new THREE.PointLight( 0xffffff, 10, 100 );
-light2.position.set( -50, 30, 50 );
+const light2 = new THREE.PointLight( 0xffffff, 20, 100 );
+light2.position.set( -2, 0, 2 );
 scene.add(light2);
 
 const light3 = new THREE.PointLight( 0xffffff, 2, 100 );
-light3.position.set( 0, 30, -5 );
+light3.position.set( 0, 3, -5 );
 scene.add(light3);
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
